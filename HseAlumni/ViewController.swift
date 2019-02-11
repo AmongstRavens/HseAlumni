4a110b58f9460b5eb15486f171e7b9a5b46bd6e3
Sergey Shevchuk <object1173.se@gmail.com>
2019-02-11 16:20:08 +0300

    Initial Commit

---
+++ HseAlumni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HseAlumni
+//
+//  Created by Sergey on 2/11/19.
+//  Copyright © 2019 Бизнес в стиле .RU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
